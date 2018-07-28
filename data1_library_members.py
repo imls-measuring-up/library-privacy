319c3315de516b3e4b2ce3afafe168ed0212a260
Patrick OBrien <patrick@revxcorp.com>
2018-07-27 17:36:19 -0700

    Renamed and documented scripts for scraping HTML of DLF, ARL, and OCLC membership and extracting research library URLs.

---
+++ data1_library_members.py
@@ -0,0 +1,41 @@
+"""
+project = "Protecting Patron Privacy on the Web: A Study of HTTPS and Google Analytics Implementation in Academic Library Websites"
+name = "data1_library_members.py",
+version = "1.0",
+author = "Patrick OBrien",
+date = "07/25/2018"
+author_email = "patrick@revxcorp.com",
+description = ("Scrape HTML of DLF, ARL, and OCLC research library membership pages."),
+license = "[MIT license](https://opensource.org/licenses/mit-license.php)",
+keywords = "IMLS Measuring Up, Digital Repositories, Research Library Privacy",
+url = "https://github.com/imls-measuring-up/library-privacy",
+"""
+
+import requests
+
+# Get Member Library Names and URLs
+data_DLF = 'www.diglib.org/members/'
+data_ARL = 'www.arl.org/membership/list-of-arl-members'
+data_OCLC = 'www.oclc.org/research/partnership/roster.html'
+
+library_list = [data_ARL, data_DLF, data_OCLC]
+
+data_dir = '_data/membership/'  # set path to write file
+
+
+# function to create file of the DLF, ARL, and OCLC membership web pages
+def web_page(members):
+    for member in members:
+        page = 'http://' + member
+        print page
+        request_page = requests.get(page, allow_redirects=True)
+        page_html = request_page.content
+        file_name_content = data_dir + member.split('/')[0] + '.html'
+        print file_name_content
+        print 'page_html: ', page_html[:200]
+        f = open(file_name_content, 'w', 1)
+        f.write(page_html)
+        f.close()
+
+
+web_page(library_list)
---
+++ data2_membership_list.py
@@ -0,0 +1,69 @@
+"""
+project = "Protecting Patron Privacy on the Web: A Study of HTTPS and Google Analytics Implementation in Academic Library Websites"
+name = "data2_membership_list.py",
+version = "1.0",
+author = "Patrick OBrien",
+date = "07/25/2018"
+author_email = "patrick@revxcorp.com",
+description = ("Extract research library URLs from DLF, ARL, and OCLC research library membership HTML pages"),
+license = "[MIT license](https://opensource.org/licenses/mit-license.php)",
+keywords = "IMLS Measuring Up, Digital Repositories, Research Library Privacy",
+url = "https://github.com/imls-measuring-up/library-privacy",
+"""
+
+import csv
+import re
+
+from bs4 import BeautifulSoup
+
+# set variables
+organizations = ['www.arl.org', 'www.diglib.org', 'www.oclc.org']
+data_directory = '_data/membership/'
+file_name = data_directory + 'data_all_library.csv'
+
+
+# open the research library membership HTML files
+def open_html_file(name):
+    open_html_file.data = open(name + '.html')  # open membership HTML page
+    open_html_file.domain = org.split('.')[1] + '.' + org.split('.')[
+        2]  # parse organization name to exclude internal website links and create filename
+    open_html_file.soup = BeautifulSoup(open_html_file.data, 'lxml')  # BeautifulSoup object containing HTML data
+
+
+# extract and write research library names and URLs to a .CSV file
+def get_urls(org, num, tags, check, writer):
+    for tag in tags[num].find_all('a'):
+        try:
+            if tag is not None:
+                next_url = tag.get('href', None).encode("utf-8")
+                if not next_url.startswith("http"):
+                    continue
+                next_name = tag.contents[0].encode("utf-8")
+                test_org = re.findall(check, next_url)
+                test_goog = re.findall('google', next_url)
+                if len(test_org) + len(test_goog) == 0:
+                    print next_name, next_url
+                    writer.writerow([org, next_name, next_url])
+        except Exception as e:
+            print
+            print "something is wrong with the HTML tag: {}".format(e)
+            print
+
+
+# create .CSV files of all the research library names and URLS extracted from DLF, ARL, and OCLC membership HTML files.
+with open(file_name, 'w') as toWrite:
+    writer = csv.writer(toWrite, delimiter=',')
+    writer.writerow(['membership', 'school', 'url'])
+    for org in organizations:
+        count = 0
+        open_html_file(org)
+        # find HTML tags encasing Membership data
+        if org == organizations[0]:
+            tags = open_html_file.soup.find_all('div', class_='article-content')  # ARL
+        elif org == organizations[1]:
+            tags = open_html_file.soup.find_all('div', class_=re.compile('one_half'))  # DLF
+        else:
+            tags = open_html_file.soup.find_all('div', class_='text parbase section')  # OCLC
+        for i in tags:
+            get_urls(org, count, tags, open_html_file.domain, writer)
+            count += 1
